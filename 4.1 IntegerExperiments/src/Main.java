18f72749c4eebfa96b0cd64b145b5bb88a7b2523
AndreyS <dmtheory1818@gmail.com>
2022-05-08 16:27:06 +0300

    Numbers

--- 4.1 IntegerExperiments/src/Main.java
+++ 4.1 IntegerExperiments/src/Main.java
@@ -4,14 +4,26 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Container container = new Container();
-        container.count += 7843;
+//        Container container = new Container();
+//        container.count += 7843;
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Imput a number: ");
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Imput a number: ");
 
-        String imput = in.nextLine();
-        sumDigits(Integer.valueOf(imput));
+//        String imput = in.nextLine();
+//        sumDigits(Integer.valueOf(imput));
+
+//        4.2
+//        напишите код, который распечатает в консоль минимальные отрицательные
+//        и максимальные положительные значения всех типов чисел.
+        System.out.println("Max Integer: " + Integer.MAX_VALUE);
+        System.out.println("Min Integer: " + Integer.MIN_VALUE);
+        System.out.println("Max Double: " + Double.MAX_VALUE);
+        System.out.println("Min Double: " + Double.MIN_VALUE);
+        System.out.println("Max Float: " + Float.MAX_VALUE);
+        System.out.println("Min Float: " + Float.MIN_VALUE);
+        System.out.println("Max Float: " + Float.MAX_VALUE);
+        System.out.println("Min Float: " + Float.MIN_VALUE);
 
     }
 
@@ -23,11 +35,19 @@ public class Main
     // 3. Выведите в консоль суммы цифр чисел: 12345, 10, 5059191.
     public static Integer sumDigits(Integer number)
     {
-        System.out.println(number.toString());
-        System.out.println(number.toString().length());
+        int sumDigits = 0;
+//        System.out.println(number.toString());
+//        System.out.println(number.toString().length());
 
+        for (int i = 0; i < number.toString().length(); i++) {
+//          int intFromImput = parseInt(number.charAt(i));
+            char numberToAdd = number.toString().charAt(i);
+            sumDigits = sumDigits + Integer.parseInt(String.valueOf(numberToAdd));
+        }
 
-        System.out.println(number);
+        System.out.println("Summary: " + sumDigits);
+
+//        System.out.println(number);
         //@TODO: write code here
         return 0;
     }
